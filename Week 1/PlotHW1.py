a44dd1c8e9d9da349db9960d5975919ce77f5dce
ObinnaEE <Obinna2013@gmail.com>
2016-01-20 22:34:24 -0500

    Update PlotHW1.py
    I hope I did this right.

--- Week 1/PlotHW1.py
+++ Week 1/PlotHW1.py
@@ -17,4 +17,30 @@
 #leave comment here with your information
 #Name:
 #Date:
+#import numpy and matlibplots module
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab as pl
+#generate array of x values from 0 to 6pi
+
+#calculate y values for sin wave
+
+#calculate y values for cosine wave
+#
+#plot x and y values for both functions 
+
+#set dashed line for sine wave and solide line for cosine wave
+
+#add legend
+
+#leave comment here with your information
+#Name: Obinna Okonkwo       
+#Date: 1/17/2016
+x = np.linspace(0, 2*3.14, 101)
+y1 = np.sin(x)
+y2 = np.cos(x)
+plt.legend(['Sin = Red, Cos = Blue'], loc='right', shadow=True)
+plt.plot(x, y1, x, y2)
+plt.title("Obi's Graph")
+plt.show()
 
